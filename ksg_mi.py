058ccb39508c863962d229b3648a5c54f73787dc
HaoRan Chang <33172704+LelouchLamperougeVI@users.noreply.github.com>
2024-12-28 22:28:40 -0500

    bug fix python wrapper
    messed up transpose

--- ksg_mi.py
+++ ksg_mi.py
@@ -4,7 +4,6 @@ from ctypes import *
 def ksg_mi(x: np.ndarray, y: np.ndarray, k=5):
     ksg = CDLL('./libksg.so').ksg
     ksg.restype = c_double
-    joint = np.hstack((x, y))
-    joint = joint.flatten()
+    joint = np.array([x, y]).T.flatten()
     I = ksg(joint.ctypes.data_as(POINTER(c_double)), int(x.size/2), k)
     return I

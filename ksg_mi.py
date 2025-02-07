ab392734c7b9854b771625ed215695a7b5405365
HaoRan Chang <haoran.chang@mail.mcgill.ca>
2025-02-07 18:11:45 +0100

    python revamp and pypi ready

--- ksg_mi.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from ctypes import *
-
-def ksg_mi(x: np.ndarray, y: np.ndarray, k=5):
-    ksg = CDLL('./libksg.so').ksg
-    ksg.restype = c_double
-    joint = np.array([x, y]).T.flatten()
-    I = ksg(joint.ctypes.data_as(POINTER(c_double)), int(x.size/2), k)
-    return I
